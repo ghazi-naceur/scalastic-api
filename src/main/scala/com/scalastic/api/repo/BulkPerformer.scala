57d0fd6e7cf3f8b3a9f7f63932942eb18e357a38
A-Hunter <ghazi.ennacer@gmail.com>
2019-04-08 23:46:50 +0100

    Bulk Processor

---
+++ src/main/scala/com/scalastic/api/repo/BulkPerformer.scala
@@ -0,0 +1,59 @@
+package com.scalastic.api.repo
+
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+
+import com.scalastic.api.client.ElasticClient
+import org.elasticsearch.action.bulk.{BackoffPolicy, BulkProcessor}
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.client.transport.TransportClient
+import org.elasticsearch.common.unit.{ByteSizeUnit, ByteSizeValue, TimeValue}
+import scala.collection.JavaConverters._
+
+/**
+  * Created by Ghazi Naceur on 08/04/2019
+  * Email: ghazi.ennacer@gmail.com
+  */
+object BulkPerformer {
+
+  var bulkRequest: BulkProcessor = _
+  val transportClient: TransportClient = ElasticClient.transportClient
+
+  private def prepareBulk(): Unit = {
+    if (bulkRequest == null) {
+      bulkRequest = BulkProcessor.builder(transportClient, new ElasticsearchBulkProcessor)
+        .setBulkActions(10000).setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB))
+        .setFlushInterval(TimeValue.timeValueSeconds(3))
+        .setBackoffPolicy(BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(1000), 3))
+        .setConcurrentRequests(1).build
+    }
+  }
+
+  def addToBulk(esIndex: String, esType: String, entity: Map[String, Any]): Unit = {
+    prepareBulk()
+    try {
+      val request = new IndexRequest(esIndex, esType, UUID.randomUUID.toString)
+      // Map (Scala) => util.Map (Java) ===> asJava (import scala.collection.JavaConverters._)
+      request.source(entity.asJava)
+      bulkRequest.add(request)
+    } catch {
+      case e: Exception => {
+        throw new Exception(e)
+      }
+    }
+  }
+
+  def closeBulk(): Unit = {
+    if (bulkRequest != null) {
+      try {
+        bulkRequest.awaitClose(Long.MaxValue, TimeUnit.MILLISECONDS)
+      }
+      catch {
+        case e: Exception => {
+          throw new Exception(e)
+        }
+      }
+      bulkRequest.close()
+    }
+  }
+}
---
+++ src/main/scala/com/scalastic/api/repo/ElasticsearchBulkProcessor.scala
@@ -0,0 +1,45 @@
+package com.scalastic.api.repo
+
+import java.util
+
+import org.elasticsearch.action.DocWriteRequest
+import org.elasticsearch.action.bulk.BulkProcessor.Listener
+import org.elasticsearch.action.bulk.{BulkRequest, BulkResponse}
+import scala.collection.JavaConverters._
+
+/**
+  * Created by Ghazi Naceur on 08/04/2019
+  * Email: ghazi.ennacer@gmail.com
+  */
+class ElasticsearchBulkProcessor extends Listener {
+  override def beforeBulk(executionId: Long, request: BulkRequest): Unit = {}
+
+  override def afterBulk(executionId: Long, request: BulkRequest, response: BulkResponse): Unit = {
+    val bulkItemResponses = response.getItems
+
+    if (bulkItemResponses.nonEmpty) {
+      for (tempResponse <- bulkItemResponses) {
+        val temp = new StringBuilder
+        if (tempResponse.isFailed) {
+          temp.append("Failed :: (").append(tempResponse.getFailureMessage).append(")").append(tempResponse.getIndex).append(" :: ").append(tempResponse.getType).append(" :: ").append(tempResponse.getId)
+        }
+        else {
+          val index = tempResponse.getIndex
+          temp.append("Success :: ").append(index).append(" :: ").append(tempResponse.getType).append(" :: ").append(tempResponse.getId)
+        }
+      }
+    }
+  }
+
+  override def afterBulk(executionId: Long, request: BulkRequest, failure: Throwable): Unit = {
+    val temp = new StringBuilder
+    val bulkItemResponses: util.List[DocWriteRequest[_]] = request.requests
+    if (bulkItemResponses != null) {
+      // util.List (Java) => List (Scala) ===> asScala (import scala.collection.JavaConverters._)
+      for (tempRequest <- bulkItemResponses.asScala) {
+        temp.append("Failed single request {}", tempRequest)
+      }
+    }
+    temp.toString()
+  }
+}
