210abcc9d3e2c916ed6d8e7aa0fefc49693d6106
ghazi-naceur <ghazi.ennacer@gmail.com>
2019-04-24 20:21:21 +0100

    Multi Document Api : multiget api

--- src/main/scala/com/scalastic/api/high/level/rest/client/document/api/MultiDocumentAPIs.scala
+++ src/main/scala/com/scalastic/api/high/level/rest/client/document/api/MultiDocumentAPIs.scala
@@ -3,6 +3,7 @@ package com.scalastic.api.high.level.rest.client.document.api
 import com.scalastic.api.client.ElasticClient
 import org.elasticsearch.action.DocWriteRequest
 import org.elasticsearch.action.bulk.{BulkRequest, BulkResponse}
+import org.elasticsearch.action.get.{MultiGetRequest, MultiGetResponse}
 import org.elasticsearch.client.{RequestOptions, RestHighLevelClient}
 
 object MultiDocumentAPIs {
@@ -15,5 +16,13 @@ object MultiDocumentAPIs {
       request.add(req)
     }
     client.bulk(request, RequestOptions.DEFAULT)
+  }
+
+  def multiGet(items: List[MultiGetRequest.Item]): MultiGetResponse = {
+    val request = new MultiGetRequest()
+    for (item <- items) {
+      request.add(item)
+    }
+    client.mget(request, RequestOptions.DEFAULT)
   }
 }
