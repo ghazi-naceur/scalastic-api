8101665f4bad4a4c59f228358b03d75afcd81d23
ghazi-naceur <ghazi.ennacer@gmail.com>
2019-04-24 18:17:28 +0200

    Single Document Api : get request

--- src/main/scala/com/scalastic/api/high/level/rest/client/document/api/SingleDocumentAPIs.scala
+++ src/main/scala/com/scalastic/api/high/level/rest/client/document/api/SingleDocumentAPIs.scala
@@ -3,9 +3,11 @@ package com.scalastic.api.high.level.rest.client.document.api
 import java.util.UUID
 
 import com.scalastic.api.client.ElasticClient
+import org.elasticsearch.action.get.GetRequest
 import org.elasticsearch.action.index.{IndexRequest, IndexResponse}
 import org.elasticsearch.client.{RequestOptions, RestHighLevelClient}
 import org.elasticsearch.common.xcontent.{XContentBuilder, XContentFactory, XContentType}
+import scala.collection.JavaConverters._
 
 object SingleDocumentAPIs {
 
@@ -33,5 +35,13 @@ object SingleDocumentAPIs {
     val request = new IndexRequest(esIndex, esType, UUID.randomUUID().toString)
     request.source(jsonSource)
     client.index(request, RequestOptions.DEFAULT)
+  }
+
+  def getById(esIndex: String, esType: String, esId: String): Map[String, Any] = {
+    val getRequest = new GetRequest(esIndex, esType, esId)
+    val response = client.get(getRequest, RequestOptions.DEFAULT)
+    // asScala : to have a mutable map
+    // map(kv => (kv._1,kv._2)).toMap : to get an immutable map
+    response.getSource.asScala.map(kv => (kv._1, kv._2)).toMap
   }
 }
--- src/test/scala/com/scalastic/api/repo/document/api/GetByIdTest.scala
+++ src/test/scala/com/scalastic/api/repo/document/api/GetByIdTest.scala
@@ -2,14 +2,14 @@ package com.scalastic.api.repo.document.api
 
 import com.scalastic.api.config.PropertiesLoader.{PERSON_INDEX, PERSON_TYPE}
 import com.scalastic.api.entities.Person
-import com.scalastic.api.repo.ElasticQueryBuilder
+import com.scalastic.api.high.level.rest.client.document.api.SingleDocumentAPIs
 
 /**
   * Created by Ghazi Naceur on 07/04/2019
   * Email: ghazi.ennacer@gmail.com
   */
 object GetByIdTest extends App {
-  val personMap = ElasticQueryBuilder.getById(PERSON_INDEX, PERSON_TYPE, "c4281022-0b2f-447d-b1c7-1e0503f78c84")
+  val personMap = SingleDocumentAPIs.getById(PERSON_INDEX, PERSON_TYPE, "570f3e69-0f9a-4f64-840a-cbd0efa2d30a")
   val person3 = Person.toPerson(personMap)
   println(person3.toString)
 
